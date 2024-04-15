037b15942b01de3263cb0ba0de76afe6ff4b9e67
Sergei <sergeyevrfr@gmail.com>
2024-04-15 15:40:09 +0400

    bebe bastards

--- src/main/java/guru/qa27/Main.java
+++ src/main/java/guru/qa27/Main.java
@@ -2,6 +2,6 @@ package guru.qa27;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello niggers!");
+        System.out.println("Hello bastards!");
     }
 }
